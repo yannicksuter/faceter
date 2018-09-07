adding correct rotation to embedding

unittests/TestLabelFaceEmbedding.py
@@ -7,19 +7,29 @@ from euclid import euclid
 import numpy as np
 import svg
 import Exporter
+import math
 
-def fit_rect_in_triangle(tri_vertice, side, rectangle):
+def angle(v1, v2):
+    angle = math.atan2(v2[1], v2[0]) - math.atan2(v1[1], v1[0])
+    if angle < 0.:
+        angle += (2. * math.pi)
+    return angle
+
+def fit_rect_in_triangle(tri_vertice, side_idx, rectangle):
     """ http://mathcountsnotes.blogspot.com/2013/05/the-largest-rectangle-inscribed-in-any.html """
     position = euclid.Point2(0., 0.)
     scaling = euclid.Point2(1., 1.)
     rotation = 0
 
-    side = euclid.Line2(euclid.Point2(tri_vertice[0].x, tri_vertice[0].y), euclid.Point2(tri_vertice[1].x, tri_vertice[1].y))
-    height = side.distance(euclid.Point2(tri_vertice[2].x, tri_vertice[2].y))
+    side = euclid.Line2(euclid.Point2(tri_vertice[side_idx%3].x, tri_vertice[side_idx%3].y), euclid.Point2(tri_vertice[(side_idx+1)%3].x, tri_vertice[(side_idx+1)%3].y))
+    height = side.distance(euclid.Point2(tri_vertice[(side_idx+2)%3].x, tri_vertice[(side_idx+2)%3].y))
 
     # Largest inscribing rectangle
     a = euclid.LineSegment2(side).length * 0.5
     b = height * 0.5
+
+    position = side.p + side.v*.5
+    rotation = angle(euclid.Vector2(side.v[1], side.v[0]), euclid.Vector2(0., rectangle._size[1]))
 
     return position, scaling, rotation
 
@@ -33,7 +43,13 @@ if __name__ == "__main__":
     # render label into path
     glyph = svg.Path.read(f'./example/svg/0123.svg', flip_y=True)
     label_path = svg.Path.combine([(glyph[int(c)], np.array([1., 0.])) for c in label])
-    fit_rect_in_triangle(vertices, 0, label_path._bbox)
+    p, s, r = fit_rect_in_triangle(vertices, 2, label_path._bbox)
+
+    # a = angle(euclid.Vector2(0, 1), euclid.Vector2(0, -2.))
+    # print(a)
+
+    label_path.rotate(r, anchor=label_path._bbox._center)
+    label_path.translate(p-label_path._bbox._center)
 
     label_path.triangulate(merge_to_model=model)
     target_path.triangulate(merge_to_model=model)