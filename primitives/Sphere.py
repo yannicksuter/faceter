594aca85b991b8edaa9c8ac0a51a530d90fb9925
yannicksuter <yannick.suter@gmail.com>
2018-09-24 12:04:45 +0200

    adding octahedron triangulation

--- primitives/Sphere.py
+++ primitives/Sphere.py
@@ -1,12 +1,94 @@
 import model
 import math
 from euclid import euclid
+import numpy as np
 
 
 class Sphere:
+    __octahedron_vertices = np.array([
+        [1.0, 0.0, 0.0],  # 0
+        [-1.0, 0.0, 0.0],  # 1
+        [0.0, 1.0, 0.0],  # 2
+        [0.0, -1.0, 0.0],  # 3
+        [0.0, 0.0, 1.0],  # 4
+        [0.0, 0.0, -1.0]  # 5
+    ])
+    __octahedron_triangles = np.array([
+        [0, 4, 2],
+        [2, 4, 1],
+        [1, 4, 3],
+        [3, 4, 0],
+        [0, 2, 5],
+        [2, 1, 5],
+        [1, 3, 5],
+        [3, 0, 5]])
+
     def __init__(self, radius, center=euclid.Vector3(0.,0.,0.)):
         self._center = euclid.Vector3(center[0], center[1], center[2])
         self._radius = radius
 
     def triangulate(self, iterations):
-        return model.Model()
+        vertex_array, index_array = self.__create_unit_sphere()
+        sphere_model = model.Model()
+        for face in index_array:
+            vertices = []
+            for v_id in face:
+                vertices.extend([vertex_array[v_id]])
+            print(vertices)
+            sphere_model.add_face(vertices)
+        return sphere_model
+
+    def __normalize_v3(self, arr):
+        ''' Normalize a numpy array of 3 component vectors shape=(n,3) '''
+        lens = np.sqrt(arr[:, 0] ** 2 + arr[:, 1] ** 2 + arr[:, 2] ** 2)
+        arr[:, 0] /= lens
+        arr[:, 1] /= lens
+        arr[:, 2] /= lens
+        return arr
+
+    def __divide_all(self, vertices, triangles):
+        # Subdivide each triangle in the old approximation and normalize
+        #  the new points thus generated to lie on the surface of the unit
+        #  sphere.
+        # Each input triangle with vertices labelled [0,1,2] as shown
+        #  below will be turned into four new triangles:
+        #
+        #            Make new points
+        #                 a = (0+2)/2
+        #                 b = (0+1)/2
+        #                 c = (1+2)/2
+        #        1
+        #       /\        Normalize a, b, c
+        #      /  \
+        #    b/____\ c    Construct new triangles
+        #    /\    /\       t1 [0,b,a]
+        #   /  \  /  \      t2 [b,1,c]
+        #  /____\/____\     t3 [a,b,c]
+        # 0      a     2    t4 [a,c,2]
+        v0 = vertices[triangles[:, 0]]
+        v1 = vertices[triangles[:, 1]]
+        v2 = vertices[triangles[:, 2]]
+        a = (v0 + v2) * 0.5
+        b = (v0 + v1) * 0.5
+        c = (v1 + v2) * 0.5
+        self.__normalize_v3(a)
+        self.__normalize_v3(b)
+        self.__normalize_v3(c)
+
+        # Stack the triangles together.
+        vertices = np.vstack((v0, b, a, b, v1, c, a, b, c, a, c, v2))
+        # Now our vertices are duplicated, and thus our triangle structure are unnecesarry.
+        return vertices, np.arange(len(vertices)).reshape((-1, 3))
+
+    def __create_unit_sphere(self, recursion_level=2):
+        vertex_array, index_array = self.__octahedron_vertices, self.__octahedron_triangles
+        for i in range(recursion_level - 1):
+            vertex_array, index_array = self.__divide_all(vertex_array, index_array)
+        return vertex_array, index_array
+
+    def __vertex_array_only_unit_sphere(self, recursion_level=2):
+        vertex_array, index_array = self.__create_unit_sphere(recursion_level)
+        if recursion_level > 1:
+            return vertex_array.reshape((-1))
+        else:
+            return vertex_array[index_array].reshape((-1))
--- unittests/TestPrimitiveSphere.py
+++ unittests/TestPrimitiveSphere.py
@@ -1,10 +1,12 @@
 import primitives
 import ObjExporter
+import numpy as np
+
+#https://sites.google.com/site/dlampetest/python/triangulating-a-sphere-recursively
 
 if __name__ == "__main__":
     sphere = primitives.Sphere(100.)
     sphere_model = sphere.triangulate(2)
 
     sphere_model.triangulate()
-    ObjExporter.write(sphere_model, f'./export/_logo_badge.obj')
-
+    ObjExporter.write(sphere_model, f'./export/_sphere.obj')
